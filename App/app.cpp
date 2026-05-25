7e8b5330fb7fcfb7e4a8a99390ccda05f08b4986
Radek Briza <radek.briza@post.cz>
2026-05-25 13:04:26 +0200

    Zmena  signalizace hladiny

--- App/app.cpp
+++ App/app.cpp
@@ -10,8 +10,16 @@
 #include "SRF05.h"
 #include "Board_hw_specific.h"
 #include "radio.h"	
+#include <span>
 
- 
+constexpr uint32_t LEVEL_L = (1u << 0);
+constexpr uint32_t LEVEL_UNDER_M = (1u << 1);
+constexpr uint32_t LEVEL_H = (1u << 2);
+
+constexpr uint32_t LEVEL_L_CM =	10;
+constexpr uint32_t LEVEL_M_CM =	30;
+constexpr uint32_t LEVEL_H_CM =	100;
+
 
 SRF05 EchoDriver(SRF05_PORT[TRIGER],SRF05_PIN[TRIGER],SRF05_PORT[ECHO],SRF05_PIN[ECHO]);
 
@@ -35,10 +43,20 @@ void App::loop()
 			if(DataTransmit::GetInstance().GetReceivedDataType() == Packet::Level_request){
 				printf("Received Level Request\n");
 				uint16_t level = static_cast<uint16_t>(EchoDriver.getCentimeter());
-				printf("Measured level: %u cm\n", level);
-				std::vector<uint8_t> payload(sizeof(level));
-				std::memcpy(payload.data(), &level, sizeof(level));
-				DataTransmit::GetInstance().SendData(Packet::Level_response, payload);
+				uint16_t status = 0;
+
+				if(level <= LEVEL_L_CM ) status  |=LEVEL_L ;
+				if(level < LEVEL_M_CM  && level >  LEVEL_L_CM) status   |=LEVEL_UNDER_M ;
+				if(level >= LEVEL_H_CM ) status  |=LEVEL_H ;
+				printf("Measured level: %u cm status :%u\n", level, status );
+    
+
+				std::vector<uint8_t> payload(sizeof(level)+sizeof(status));
+				auto LevelArrea =  std::span{payload}.first(sizeof(level)); 
+				auto StatusArrea = std::span{payload}.subspan(sizeof(status), sizeof(level)); 
+				std::memcpy(LevelArrea.data(), &level, sizeof(level));
+				std::memcpy(StatusArrea.data(),&status, sizeof(status ));
+ 				DataTransmit::GetInstance().SendData(Packet::Level_response, payload);
 			}
 			else if(DataTransmit::GetInstance().GetReceivedDataType() == Packet::Battery_request){
 				printf("Received Battery Request\n");
